38f0068c4530a232dfad76e1e87a1122d0882ff4
azzamallow <azzamallow@gmail.com>
2013-12-20 11:00:24 -0500

    watch when testing

--- karma.conf.js
+++ karma.conf.js
@@ -19,8 +19,7 @@ module.exports = function(config) {
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/scripts/*.coffee',
       'app/scripts/**/*.coffee',
-      'test/mock/**/*.coffee',
-      'test/spec/**/*.coffee'
+      'test/spec/*.coffee'
     ],
 
     // list of files / patterns to exclude
@@ -35,7 +34,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // Start these browsers, currently available:
